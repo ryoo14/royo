ad249e030899b33f8cea34467407bd477cfdb121
ryoo14 <anana12185@gmail.com>
2024-09-07 02:23:04 +0900

    feat: deadline input can now be selected from the calendar.

--- components.tsx
+++ components.tsx
@@ -7,6 +7,7 @@ export const renderer = jsxRenderer(({ children }) => {
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
         <script src="https://unpkg.com/htmx.org@2.0.2" integrity="sha384-Y7hw+L/jvKeWIRRkqWYfPcvVxHzVzn5REgzbawhxAuQGwX1XWe70vji+VSeHOThJ" crossorigin="anonymous"></script>
         <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
         <script src="https://cdn.tailwindcss.com"></script>
@@ -23,6 +24,13 @@ export const renderer = jsxRenderer(({ children }) => {
         <div id="container" class="flex justify-center items-center w-full h-full">
           ${children}
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+        <script>
+          flatpickr("#deadline", {
+            enableTime: true,
+            dateFormat: "Y-m-d H:i"
+          })
+        </script>
       </body>
     </html>
   `
@@ -32,7 +40,7 @@ export const AddToDo = () => (
   <form hx-post="/todos" hx-target="#todo" hx-swap="beforebegin" _="on htmx:afterRequest reset() me" class="flex flex-row justify-around w-full mb-10">
     <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-1/5" name="category" type="text" />
     <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-1/5" name="content" type="text" />
-    <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-1/5" name="deadline" type="text" />
+    <input id="deadline" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-1/5" name="deadline" type="text" />
     <button class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-1/5" type="submit">
       Submit
     </button>
